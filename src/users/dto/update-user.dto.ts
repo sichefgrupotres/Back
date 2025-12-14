70e9874ecc2eba053dc667ac9db1796f0accd4fc
pabloaliano <pabloaliano@gmail.com>
2025-12-14 13:54:49 -0300

    DTOs modificacion pedida para entidad y carp decorators

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(
+  PickType(CreateUserDto, [
+    'password',
+    'cumpleaños',
+    'avatarUrl',
+    'nacionalidad',
+    'genero',
+    'paisDeResidencia',
+    'ciudad',
+  ] as const),
+) {}
