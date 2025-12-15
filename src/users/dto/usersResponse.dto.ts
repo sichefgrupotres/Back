181ba11010d68dbb3a8021e9a5524606f2ee15e8
orlandozarraga <orlandozarraga31@hotmail.com>
2025-12-15 16:40:18 -0300

    Actulizaciones a Modulo Users

---
+++ src/users/dto/usersResponse.dto.ts
@@ -0,0 +1,6 @@
+export class UserResponseDto {
+  id: string;
+  nombre: string;
+  apellido: string;
+  email: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './users.repository';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserResponseDto } from './dto/usersResponse.dto';
 
 @Injectable()
 export class UsersService {
@@ -21,10 +22,24 @@ export class UsersService {
     return this.usersRepository.findOne(id);
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return this.usersRepository.update(id, updateUserDto);
-  }
+  async update(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseDto> {
+    await this.usersRepository.update(id, updateUserDto);
+    const user = await this.usersRepository.findOne(id);
 
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return {
+      id: user.id,
+      nombre: user.nombre,
+      apellido: user.apellido,
+      email: user.email,
+    };
+  }
   remove(id: string) {
     return `This action removes a #${id} user`;
   }
