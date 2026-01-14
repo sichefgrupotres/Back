26cc6e5c7eba0577d05f01603b217e4b992068e3
CarolinaAT <carolinaarizat93@gmail.com>
2026-01-14 01:14:30 -0300

    Actualizacion en el modulo de app

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,42 +18,42 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
     }),
     EventEmitterModule.forRoot(),
     //EN ESTAPA DE DESAROLLO (DESCOMENTAR ESTA PARTE)
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (config: ConfigService) => ({
-    //     type: 'postgres',
-    //     host: config.get('DB_HOST'),
-    //     port: Number(config.get('DB_PORT')),
-    //     username: config.get('DB_USER'),
-    //     password: config.get('DB_PASSWORD'),
-    //     database: config.get('DB_NAME'),
-    //     autoLoadEntities: true,
-    //     synchronize: process.env.NODE_ENV !== 'production',
-    //     ssl:
-    //       process.env.NODE_ENV === 'production'
-    //         ? { rejectUnauthorized: false }
-    //         : false,
-    //     dropSchema: false,
-    //     logging: true,
-    //   }),
-    // }),
-    // EN PRODUCCION (DESCOMENTAR ESTA PARTE)
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        url: config.get<string>('DATABASE_URL'),
+        host: config.get('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: process.env.NODE_ENV !== 'production',
         ssl:
-          config.get('NODE_ENV') === 'production'
+          process.env.NODE_ENV === 'production'
             ? { rejectUnauthorized: false }
             : false,
+        dropSchema: false,
         logging: true,
       }),
     }),
+    // EN PRODUCCION (DESCOMENTAR ESTA PARTE)
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => ({
+    //     type: 'postgres',
+    //     url: config.get<string>('DATABASE_URL'),
+    //     autoLoadEntities: true,
+    //     synchronize: true,
+    //     ssl:
+    //       config.get('NODE_ENV') === 'production'
+    //         ? { rejectUnauthorized: false }
+    //         : false,
+    //     logging: true,
+    //   }),
+    // }),
 
     UsersModule,
 
